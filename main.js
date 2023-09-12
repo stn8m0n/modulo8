750c43bfd76753674520db9c47d68ccae983d558
Maicon Santana <msn-maicon@hotmail.com>
2023-09-12 03:34:20 -0300

    testeFuncional01

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
-const form = document.getElementById('form-atividades');
+const form = document.getElementById('form-atividade');
 form.addEventListener('submit' , function(e){
     e.preventDefault();
     const inputNomeAtividade = document.getElementById ('nome-atividade');
     const inputNotaAtividade = document.getElementById ('nota-atividade');
 
-    alert (`Atividade: ${inputNomeAtividade.value} - Nota: ${inputNotaAtividade.value} `);
+    alert(`Atividade: ${inputNomeAtividade.value} - Nota: ${inputNotaAtividade.value} `);
 
-})
+});
