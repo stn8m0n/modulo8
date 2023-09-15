284d56fe2fe5b74ee3f2a019f630463794fedc46
Maicon Santana <msn-maicon@hotmail.com>
2023-09-15 04:43:36 -0300

    calcula02

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
 const form = document.getElementById('form-atividade');
 const imgAprovado = '<img src="./images/aprovado.png" alt="Celebrando" />';
 const imgReprovado = '<img src="./images/reprovado.png" alt="decepcionado" />';
+const atividades = [];
+const notas = [];
+/*const spanAprovado =´<span class="resultado aprovado"´></span>*/
 
 let linhas = '';
 
@@ -9,13 +12,17 @@ form.addEventListener('submit' , function(e){
 
     adicionaLinha();
     atualizaTabela();
+    atualizaMediaFinal();
 
-});
+})
 
 function adicionaLinha () {
 
     const inputNomeAtividade = document.getElementById ('nome-atividade');
     const inputNotaAtividade = document.getElementById ('nota-atividade');
+
+    atividades.push(inputNomeAtividade.value);
+    notas.push(parseFloat(inputNotaAtividade.value));
 
     let linha = '<tr>';
     linha += `<td>${inputNomeAtividade.value} </td>`;
@@ -35,4 +42,23 @@ function adicionaLinha () {
 function atualizaTabela (){
     const corpoTabela = document.querySelector ('tbody');
     corpoTabela.innerHTML = linhas;
+}
+
+function atualizaMediaFinal(){
+    const mediaFinal = calculaMediaFinal();
+    
+    document.getElementById('media-final-valor').innerHTML = mediaFinal;
+    document.getElementById('media-final-resultado').innerHTML = mediaFinal >= 7 ? 'Aprovado' : 'Reprovado';
+}
+
+function calculaMediaFinal(){
+    
+        let somaDasNotas = 0;
+
+    for (let i = 0; i < notas.length; i++) {
+        somaDasNotas += notas[i];
+    } 
+
+     return somaDasNotas / notas.length;
+    
 }
